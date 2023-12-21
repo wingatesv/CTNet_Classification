2a82d02c950d948014ed7350e58aa2f48fed9ae4
Wingates Voon <56868536+wingatesv@users.noreply.github.com>
2023-12-21 17:10:38 +0800

    Update train.py

--- train.py
+++ train.py
@@ -85,7 +85,7 @@ for epoch in range(total_epochs):
                 
                 log.info('Save checkpoints: epoch = {}, batch_id = {}'.format(epoch, batch_id)) 
                 torch.save({
-                            'ecpoch': epoch,
+                            'epoch': epoch,
                             'batch_id': batch_id,
                             'state_dict': model.state_dict(),
                             'optimizer': optimizer.state_dict()},
@@ -94,55 +94,56 @@ for epoch in range(total_epochs):
 print('Finished training')            
 if sets.ci_test:
     exit()
-if name == 'main':
-# settting
-sets = parse_opts()
-if sets.ci_test:
-sets.img_list = './toy_data/test_ci.txt'
-sets.n_epochs = 1
-sets.no_cuda = True
-sets.data_root = './toy_data'
-sets.pretrain_path = ''
-sets.num_workers = 0
-sets.model_depth = 10
-sets.resnet_shortcut = 'A'
-sets.input_D = 14
-sets.input_H = 28
-sets.input_W = 28
-
-# getting model
-torch.manual_seed(sets.manual_seed)
-model, parameters = generate_model(sets) 
-print (model)
-# optimizer
-if sets.ci_test:
-    params = [{'params': parameters, 'lr': sets.learning_rate}]
-else:
-    params = [
-            { 'params': parameters['base_parameters'], 'lr': sets.learning_rate }, 
-            { 'params': parameters['new_parameters'], 'lr': sets.learning_rate*100 }
-            ]
-optimizer = torch.optim.SGD(params, momentum=0.9, weight_decay=1e-3)   
-scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
-
-# train from resume
-if sets.resume_path:
-    if os.path.isfile(sets.resume_path):
-        print("=> loading checkpoint '{}'".format(sets.resume_path))
-        checkpoint = torch.load(sets.resume_path)
-        model.load_state_dict(checkpoint['state_dict'])
-        optimizer.load_state_dict(checkpoint['optimizer'])
-        print("=> loaded checkpoint '{}' (epoch {})"
-          .format(sets.resume_path, checkpoint['epoch']))
-
-# getting data
-sets.phase = 'train'
-if sets.no_cuda:
-    sets.pin_memory = False
-else:
-    sets.pin_memory = True    
-training_dataset = BrainS18Dataset(sets.data_root, sets.img_list, sets)
-data_loader = DataLoader(training_dataset, batch_size=sets.batch_size, shuffle=True, num_workers=sets.num_workers, pin_memory=sets.pin_memory)
-
-# training
-train(data_loader, model, optimizer, scheduler, total_epochs=sets.n_epochs, save_interval=sets.save_intervals, save_folder=sets.save_folder, sets=sets) 
+    
+if __name__ == '__main__':
+    # settting
+    sets = parse_opts()
+    if sets.ci_test:
+    sets.img_list = './toy_data/test_ci.txt'
+    sets.n_epochs = 1
+    sets.no_cuda = True
+    sets.data_root = './toy_data'
+    sets.pretrain_path = ''
+    sets.num_workers = 0
+    sets.model_depth = 10
+    sets.resnet_shortcut = 'A'
+    sets.input_D = 14
+    sets.input_H = 28
+    sets.input_W = 28
+    
+    # getting model
+    torch.manual_seed(sets.manual_seed)
+    model, parameters = generate_model(sets) 
+    print (model)
+    # optimizer
+    if sets.ci_test:
+        params = [{'params': parameters, 'lr': sets.learning_rate}]
+    else:
+        params = [
+                { 'params': parameters['base_parameters'], 'lr': sets.learning_rate }, 
+                { 'params': parameters['new_parameters'], 'lr': sets.learning_rate*100 }
+                ]
+    optimizer = torch.optim.SGD(params, momentum=0.9, weight_decay=1e-3)   
+    scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+    
+    # train from resume
+    if sets.resume_path:
+        if os.path.isfile(sets.resume_path):
+            print("=> loading checkpoint '{}'".format(sets.resume_path))
+            checkpoint = torch.load(sets.resume_path)
+            model.load_state_dict(checkpoint['state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (epoch {})"
+              .format(sets.resume_path, checkpoint['epoch']))
+    
+    # getting data
+    sets.phase = 'train'
+    if sets.no_cuda:
+        sets.pin_memory = False
+    else:
+        sets.pin_memory = True    
+    training_dataset = BrainS18Dataset(sets.data_root, sets.img_list, sets)
+    data_loader = DataLoader(training_dataset, batch_size=sets.batch_size, shuffle=True, num_workers=sets.num_workers, pin_memory=sets.pin_memory)
+    
+    # training
+    train(data_loader, model, optimizer, scheduler, total_epochs=sets.n_epochs, save_interval=sets.save_intervals, save_folder=sets.save_folder, sets=sets) 
